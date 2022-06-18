1f514db1d415291926072439a7d0aa89054be88c
GeorgyRad <g.e.radkov08092000@yandex.ru>
2022-06-18 15:08:07 +0300

    Initial commit

---
+++ task3.py
@@ -0,0 +1,35 @@
+"""Пользователь вводит месяц в виде целого числа от 1 до 12.
+Сообщить к какому времени года относится месяц (зима, весна, лето, осень).
+Напишите решения через list и через dict."""
+
+#через словарь
+year = {
+    1: "winter",
+    2: "winter",
+    3: "spring",
+    4: "spring",
+    5: 'spring',
+    6: "summer",
+    7: "summer",
+    8: "summer",
+    9: "autumn",
+    10: "autumn",
+    11: "autumn",
+    12: "winter"
+}
+mounth = int(input("Введите номер месяца: "))
+if mounth > 12 or mounth <= 0:
+    print("неверное число")
+else:
+    print(year.get(mounth))
+
+#через список
+list = [
+    '_',
+    'winter', 'winter',
+    'spring', 'spring', 'spring',
+    'summer', 'summer', 'summer',
+    'autunm', 'autumn', 'autumn',
+    'winter'
+]
+print(list[mounth])
